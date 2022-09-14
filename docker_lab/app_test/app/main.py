085ad95a46f2f4d47fde601e00b667745f479ebb
Enrique Mora <enrique.mora@es.nestle.com>
2022-09-14 12:29:32 +0200

    docker_labs

---
+++ docker_lab/app_test/app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
+
+@app.get("/get-iris")
+def get_iris():
+
+    import pandas as pd
+    url ='https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+    iris = pd.read_csv(url)
+
+    return iris
+
+@app.get("/plot-iris")
+def plot_iris():
+
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    url ='https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv'
+    iris = pd.read_csv(url)
+
+    plt.scatter(iris['sepal_length'], iris['sepal_width'])
+    plt.savefig('iris.png')
+    file = open('iris.png', mode="rb")
+
+    return StreamingResponse(file, media_type="image/png")
